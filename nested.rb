9a35441fd751754b7e08179c4a67a604abab52f5
Anthony Joseph Marchitelli Jr <amarchitelli1993@gmail.com>
2019-07-10 05:58:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,9 +20,8 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	#What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	programmer_hash[:alan_kay][:known_for]
-	end
+end
 	programmer_hash = 
  		{
         :grace_hopper => {
